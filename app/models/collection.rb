84eeacd55d1be5b99c0e363a07064277dacf18e8
Shou Ya <github@lain.li>
2016-09-14 16:14:48 +0800

    Add basic validations

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,6 @@
 class Collection < ApplicationRecord
   has_and_belongs_to_many :topics
+
+  validates_presence_of :title
+  validates_presence_of :description
 end
--- app/models/column.rb
+++ app/models/column.rb
@@ -2,4 +2,7 @@ class Column < ApplicationRecord
   has_many :topics
 
   enum type: [:normal, :video]
+
+  validates_presence_of :title
+  validates_presence_of :description
 end
