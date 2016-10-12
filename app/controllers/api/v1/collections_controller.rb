55c90ed7ee304fce15c39bf56ee440e4a4bd739a
Shou Ya <github@lain.li>
2016-10-12 15:55:37 +0800

    basic collection actions

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,8 +1,34 @@
 module API::V1
   class CollectionsController < APIController
+    before_action :find_collection, only: %i(destroy show)
+
     def index
-      collections = Collection.all
-      render(json: collections)
+      success(Collection.all)
+    end
+
+    def create
+      collection = Collection.create(*collection_params)
+      created(collection)
+    end
+
+    def destroy
+      @collection.destroy
+      updated
+    end
+
+    def show
+      success(@collection)
+    end
+
+    private
+
+    def find_collection
+      @collection = Collection.find(params[:id])
+    end
+
+    def collection_params
+      params.require(:title)
+            .permit(:description, :banner, :banner_mobile, :meta)
     end
   end
 end
