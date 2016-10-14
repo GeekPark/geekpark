bf6d9b61fefb050b220a20dcadab0987409429e9
Shou Ya <github@lain.li>
2016-10-14 23:22:24 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    namespace :v1 do
-      resources :collections, only: [:index]
+    namespace :v1, except: [:new, :edit] do
+      resources :collections do
+        post :reset_members, :add_members
+      end
     end
   end
 end
