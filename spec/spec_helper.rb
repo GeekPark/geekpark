f3fce817b5ebf5cbbd1ec38d11391a25ddbbf60a
Shou Ya <github@lain.li>
2016-10-14 23:44:50 +0800

    local coverage reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,10 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'codeclimate-test-reporter'
+require 'simplecov'
+
 CodeClimate::TestReporter.start
+SimpleCov.start
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
