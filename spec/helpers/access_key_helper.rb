d438446465cdb0011c7d2f59dfb3cff68fad3c2f
Shou Ya <github@lain.li>
2016-10-17 16:11:57 +0800

    remove useless code

--- spec/helpers/access_key_helper.rb
+++ spec/helpers/access_key_helper.rb
@@ -21,10 +21,6 @@ module AccessKeyHelper
 
   Rack::ExtractUser.prepend MockRemoteStorage
 
-  def access_key_param_for(*args)
-    { access_key: access_key_for(*args) }
-  end
-
   def access_key_for(role = nil, id: rand(100_000), roles: [])
     roles = [role] if roles.empty?
 
