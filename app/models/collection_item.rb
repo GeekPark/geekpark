bef16b1ca290b524b408c82d3c31ade76d43e06d
Shou Ya <github@lain.li>
2016-10-18 14:28:54 +0800

    annotate models

--- app/models/collection_item.rb
+++ app/models/collection_item.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: collection_items
+#
+#  id            :integer          not null, primary key
+#  collection_id :integer
+#  topic_id      :integer
+#
+# Indexes
+#
+#  index_collection_items_on_collection_id  (collection_id)
+#  index_collection_items_on_topic_id       (topic_id)
+#
+
 class CollectionItem < ApplicationRecord
   belongs_to :collection
   belongs_to :topic
