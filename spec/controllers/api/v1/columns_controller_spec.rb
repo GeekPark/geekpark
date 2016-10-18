9796ad3a3fe77545ffb3856b0e8a2080c8bae5c4
Shou Ya <github@lain.li>
2016-10-18 17:55:03 +0800

    test for column#add_members

--- spec/controllers/api/v1/columns_controller_spec.rb
+++ spec/controllers/api/v1/columns_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe API::V1::ColumnsController, type: :controller do
   it_behaves_like(:indicable, Column, as: :editor)
+  it_behaves_like(:destructible, :column, as: :admin)
   it_behaves_like(:creatable, Column, as: :admin) do
     let(:query) {
       Hash[
@@ -26,5 +27,23 @@ RSpec.describe API::V1::ColumnsController, type: :controller do
     }
   end
 
-  it_behaves_like(:destructible, :column, as: :admin)
+  describe 'add_members' do
+    it 'adds members successfully' do
+      n = 3
+      column = create(:column)
+      topics = create_list(:topic, n)
+
+      expect {
+        post :add_members,
+             params: {
+               column_id: column.id,
+               topic_ids: topics.pluck(:id),
+               **as(:editor),
+               format: :json
+             }
+        expect(response).to be_no_content
+      }.to change { column.topics.count }.by(n)
+      expect(column.topics).to include(*topics)
+    end
+  end
 end
