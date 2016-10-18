273405c7bb04c08e9858608a304e895f647b5467
Shou Ya <github@lain.li>
2016-10-18 14:30:16 +0800

    annotate serializers

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: collections
+#
+#  id            :integer          not null, primary key
+#  title         :string
+#  description   :string
+#  banner        :string
+#  banner_mobile :string
+#  meta          :hstore
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  deleted_at    :datetime
+#
+# Indexes
+#
+#  index_collections_on_deleted_at  (deleted_at)
+#  index_collections_on_meta        (meta)
+#  index_collections_on_title       (title)
+#
+
 class CollectionSerializer < ApplicationSerializer
   attributes :id, :title, :description
   attribute :meta, if: :manager?
