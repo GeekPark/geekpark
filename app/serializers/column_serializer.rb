24cc552318112da913218f6d91f114dfe95f7a32
Shou Ya <github@lain.li>
2016-10-20 15:52:12 +0800

    annotate serializers

--- app/serializers/column_serializer.rb
+++ app/serializers/column_serializer.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: columns
+#
+#  id           :integer          not null, primary key
+#  title        :string
+#  description  :string
+#  meta         :hstore
+#  content_type :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  deleted_at   :datetime
+#
+# Indexes
+#
+#  index_columns_on_content_type  (content_type)
+#  index_columns_on_deleted_at    (deleted_at)
+#  index_columns_on_meta          (meta)
+#  index_columns_on_title         (title)
+#
+
 class ColumnSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :meta
 
