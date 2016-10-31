da1cca26ff96f105dff7531ab76991b4839b0b82
Shou Ya <github@lain.li>
2016-10-31 15:52:01 +0800

    working on header bar

--- front-end/src/pages/header/index.js
+++ front-end/src/pages/header/index.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
+import './index.scss';
+
+class Tab extends Component {
+  render() {
+    return (
+      <a href={this.props.href} className={"nav-link " + (this.props.active && 'active')}>
+        {this.props.label}
+      </a>
+    )
+  }
+}
+
 export default class Header extends Component {
   render() {
     return (
       <header className="header">
-      Header
+        <span className="logo">
+          <img src={require('../../img/logo-white.png')} />
+        </span>
+        <nav className="nav">
+          <Tab label="資訊" href="/" active={true}/>
+          <Tab label="視頻" href="/videos" />
+          <Tab label="活動" href="//events.geekpark.net" />
+          <Tab label="極客加速" href="//geeks.vc" />
+        </nav>
+        <span className="login-btn">登入/注册</span>
       </header>
     );
   }
