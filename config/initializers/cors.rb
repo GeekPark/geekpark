07d0390cf87dea13e72e0454e1090419f02d3ce1
Shou Ya <github@lain.li>
2017-05-10 14:57:44 +0800

    dynamically configured cors on sandbox

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,9 @@
 middleware = Rails.application.config.middleware
 middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: :get
-  end
-
-  allow do
-    origins '*'              if Rails.env.development?
-    origins '*.geekpark.net' if Rails.env.production?
-
+    origins SiteConfig.cors_allowed_origins
     resource '*',
              headers: :any,
-             methods: %i[post put patch delete options head]
+             methods: %i[get post put patch delete options head]
   end
 end
