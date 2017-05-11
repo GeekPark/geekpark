e212a27e70c51be23b91d5c8c4a9040b8aa90ebc
bomb <476964702@qq.com>
2017-05-11 12:02:31 +0800

    refactor countable

--- app/models/concerns/countable.rb
+++ app/models/concerns/countable.rb
@@ -18,14 +18,14 @@ module Countable
       define_singleton_method "order_by_#{action}_count" do
           |length = 1.week, desc = true|
         all.joins(:"#{action}_counts")
-           .where(published_at: (Time.now - length)..Time.now)
+           .where(published_at: length.ago..Time.now)
            .group(:id)
            .order("sum(counts.count) #{'desc' if desc}")
       end
 
       define_singleton_method "count_#{action.pluralize}" do |length = 1.week|
         all.joins(:"#{action}_counts")
-           .where(published_at: (Time.now - length)..Time.now)
+           .where(published_at: length.ago..Time.now)
            .sum('counts.count')
       end
     end
