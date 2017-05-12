0ae1e056b24bc1a4b8d138065b2d7858de444492
bomb <476964702@qq.com>
2017-05-12 16:57:57 +0800

    replace before(:all) with before(:each)

--- spec/models/concerns/countable_spec.rb
+++ spec/models/concerns/countable_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Countable do
-  before(:all) do
+  before(:each) do
     create_list(:post, 20)
     @post = Post.last
   end
--- spec/models/concerns/smart_filterable_spec.rb
+++ spec/models/concerns/smart_filterable_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe SmartFilterable do
-  before(:all) do
+  before(:each) do
     create(:comment)
     create(:post)
   end
