090d483cc0ed814785ae1ac650129deb099bcb8c
bomb <476964702@qq.com>
2017-05-15 09:19:21 +0800

    change action name to avoid name conflict

--- spec/controllers/api/v1/admin/admin_controller_spec.rb
+++ spec/controllers/api/v1/admin/admin_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe API::V1::Admin::AdminController, type: :controller do
   controller do
-    def test
+    def raise_recordinvalid
       test_post = Post.new
       test_post.errors.add(:test, "test")
       raise ActiveRecord::RecordInvalid, test_post
@@ -11,8 +11,10 @@ describe API::V1::Admin::AdminController, type: :controller do
 
   describe "handling ActiveRecord::RecordInvalid exceptions" do
     it "should properly return error info" do
-      routes.draw { get "test" => "api/v1/admin/admin#test" }
-      get :test
+      routes.draw {
+        get "raise_recordinvalid" => "api/v1/admin/admin#raise_recordinvalid"
+      }
+      get :raise_recordinvalid
       expect(response).to be_unprocessable
       result = '{"errors":{"type":"invalid_record",' \
                '"info":{"test":["test"]},"message":null}}'
